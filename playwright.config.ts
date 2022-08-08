8527caffcc9f2eac59a8fab82921505ca4c82d5b
Astemir Boziev <bootuz07@gmail.com>
2022-08-08 16:47:13 +0400

    added linter

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
     workers: 4,
     reporter: [['html', { open: 'always' }], ['list']],
     use: {
-        actionTimeout: 10000,
+        actionTimeout: 15000,
         baseURL: 'https://data.fundraiseup.com',
         headless: false,
         screenshot: 'only-on-failure',
